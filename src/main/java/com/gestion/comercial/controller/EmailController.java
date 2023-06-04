723e78ebee6842983ebdbaa722262fcc644c6f12
FOMonti <montorofernando4315@gmail.com>
2023-06-04 20:57:53 -0300

    agrego mensaje al retornar un 200 cuando se envia un email

--- src/main/java/com/gestion/comercial/controller/EmailController.java
+++ src/main/java/com/gestion/comercial/controller/EmailController.java
@@ -1,7 +1,12 @@
 package com.gestion.comercial.controller;
 
+import com.gestion.comercial.dto.ClienteResponse;
+import com.gestion.comercial.dto.CustomErrorResponse;
 import com.gestion.comercial.service.EmailService;
 import com.itextpdf.text.DocumentException;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,13 +28,18 @@ public class EmailController {
         this.emailService = emailService;
     }
 
+
     @PostMapping("/enviar-pdf")
+    @ApiResponse(responseCode = "200", description = "Email enviado exitosamente",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud",
+            content = @Content(mediaType = "application/json", schema = @Schema (implementation = CustomErrorResponse.class)))
     public ResponseEntity<String> enviarPDF(@RequestParam String emailReceptor, @RequestParam Long idCotizacionVenta){
         try {
             emailService.enviarCotizacion(emailReceptor,idCotizacionVenta);
         }catch (DocumentException | IOException e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al enviar el PDF por correo electrónico.");
         }
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("Email enviado exitosamente");
     }
 }
